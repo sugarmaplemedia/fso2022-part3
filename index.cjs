fc2d5fe1b8fd9d1edb21ae65434a6b228e3e1e9f
Harrison Bouche <harrison@sm.media>
2023-02-08 10:36:57 -0500

    Cleaned up disasterously messy code

--- index.cjs
+++ index.cjs
@@ -4,7 +4,7 @@ const morgan = require("morgan")
 
 const app = express()
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 morgan.token("body", (req, res) => req.method === "POST" ? `- ${JSON.stringify(req.body)}` : " ")
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
